271b63b3ae410dd8f20009340998c7e0574e41e7
besSejrani <sejrani.besjan@gmail.com>
2021-06-08 13:50:13 +0200

    modified folder structure

---
+++ Documentation/docs/a.Standard Template Library/STL.md
@@ -0,0 +1,20 @@
+# STL
+
+<hr/>
+
+## Containers
+
+Collection of objects or primitive types (array, vector, deque, stack, set, map, ...)
+
+<hr/>
+## Algorithms
+
+Functions for processing sequences of elements from containers (find, max, count, accumulate, sort, ...)
+
+<hr/>
+
+## Iterators
+
+Generate sequences of element from containers (forward, reverse, by value, by reference, constant, ...)
+
+<hr/>
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,34 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include "./Class/Player/Player.h"
 
-
-
 int main() {
+  Player p1;
+  Player p2;
+  Player *ennemy = new Player;
+  ennemy->talk();
+  delete ennemy;
 
-    Player p1;
-    Player p2;
-    Player *ennemy = new Player;
-    ennemy->talk();
-    delete ennemy;
+  std::vector<Player> players{p1, p2};
 
-    std::vector<Player> players {p1,p2};
+  p1.talk();
+  p1.bla();
 
-    p1.talk();
-    p1.bla();
+  int r = 99;
+  int *bla = {&r};
 
-    int r=99;
-    int *bla = {&r};
+  int tada = *bla;
 
-    int tada = *bla;
+  int myNumber = 0;
+  int myPointer = 10;
 
-    int myNumber=0;
-    int myPointer = 10;
+  std::cout << tada << std::endl;
 
+  std::cout << *&*&myPointer << std::endl;
 
-    std::cout<<tada<<std::endl;
+  std::cout << "hello world" << std::endl;
 
-    std::cout<<*&*&myPointer<<std::endl;
-
-    std::cout << "hello world" << std::endl;
-
-    
-    return 0;
+  return 0;
 }
